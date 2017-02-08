89a25ef0f1b03672b202d5880d0cf6b3ce895b38
Sasan Bahadaran <sbahadaran@doc.gov>
2017-02-07 23:28:28 -0500

    added scraping functions for pages within parent topic; still need to combine with child topics into single file output

--- census_topic_crawler/items.py
+++ census_topic_crawler/items.py
@@ -8,12 +8,30 @@
 import scrapy
 
 
-class Topic(scrapy.Item):
-    parent_topic_ID = scrapy.Field()
-    parent_topic_name = scrapy.Field()
+class ParentTopic(scrapy.Item):
     topic_type = scrapy.Field()
-    topic_name = scrapy.Field()
-    topic_link = scrapy.Field()
+    ID = scrapy.Field()
+    name = scrapy.Field()
+    link = scrapy.Field()
+    main_content = scrapy.Field()
+    about_content = scrapy.Field()
+    #list of dicts with each containing - name, description
+    about_child_topics = scrapy.Field()
+    #list of dicts with each containing - date, title, description, link
+    news_items = scrapy.Field()
+    #list of dicts with each containing - title, description, link
+    survey_items = scrapy.Field()
+    #survey_title = scrapy.Field()
+    #survey_content = scrapy.Field()
+
+class ChildTopic(scrapy.Item):
+    #parent_topic_ID = scrapy.Field()
+    #parent_topic_name = scrapy.Field()
+    #topic_type = scrapy.Field()
+    #topic_name = scrapy.Field()
+    #topic_link = scrapy.Field()
+    topic_type = scrapy.Field()
+    link = scrapy.Field()
     main_title = scrapy.Field()
     main_content = scrapy.Field()
     related_sites = scrapy.Field()
@@ -21,15 +39,13 @@ class Topic(scrapy.Item):
     about_content = scrapy.Field()
     topic_faq_content = scrapy.Field()
 
-#class AboutsubTopic(scrapy.Item):
+# #for parent topic about page. One item for each child topic
+# class ParentAboutChildTopics(scrapy.Item):
+#     child_topic_name = scrapy.Field()
+#     child_topic_description = scrapy.Field()
 
-#parent_topic_about_title
-#parent_topic_about_content
-#parent_topic_about_subtopic (need a new item per with title and description)
-
-# class AboutPage(scrapy.Item):
-#     # define the fields for your item here like:
-#     # name = scrapy.Field()
-#     name = scrapy.Field()
-#     title = scrapy.Field()
-#     content = scrapy.Field()
+#for parent topic news page.  One item for each article
+# class ParentNewsArticle(scrapy.Item):
+#     news_article_year = scrapy.Field()
+#     news_article_title = scrapy.Field()
+#     news_article_description = scrapy.Field()
--- census_topic_crawler/spiders/census_topic_crawler.py
+++ census_topic_crawler/spiders/census_topic_crawler.py
@@ -6,57 +6,115 @@
 #Description:   This is a script for scraping topics and related information
 #from the census.gov website
 
-import scrapy
-from census_topic_crawler.items import Topic
+import scrapy, datetime, itertools
+from census_topic_crawler.items import ParentTopic, ChildTopic
 
 
 class MySpider(scrapy.Spider):
     name = "census"
     start_urls = [
-            'http://www.census.gov/topics.html',
+            'http://www.census.gov/topics.html'
             #'http://www.census.gov/topics/preparedness.html'
     ]
 
     #rules = [Rule(LinkExtractor(allow=['/technology-\d+']), 'parse_story')]
 
     def parse(self, response):
-        #get topics
-        #use
-        item = Topic()
+        print('IN PARSE')
         parent_topic_ids = response.xpath('//div[contains(@id, "accd_a") and not (@id="NAV_437121255_0_accd_a")]/@id').extract()
-        for parent_topic_id in parent_topic_ids:
-            parse_parent(parent_topic_id)
-            item['parent_topic_ID'] = parent_topic_id
+        item = ParentTopic()
+        print('parent_topic_ids: {}'.format(parent_topic_ids))
+        for parent_topic_id in itertools.islice(parent_topic_ids, 0, 2):
+            print('IN FOR!!!!!!')
+            item['topic_type'] = 'parent'
+            item['ID'] = parent_topic_id
             print('parent_topic_id: {}'.format(parent_topic_id))
             parent_topic_name = response.xpath('//div[@id="'+parent_topic_id+'"]//h4[@class="mobileMenuEntry2"]/text()').extract()[0].strip()
             print('parent_topic_name: {}'.format(parent_topic_name))
-            item['parent_topic_name'] = parent_topic_name
-            topic_links = response.xpath('//div[@id="'+parent_topic_id[:-2]+'"]//a/@href').extract()
-            for topic_link in topic_links:
-                #call function for topic
-                item['topic_link'] = topic_link
-                print('topic_link: {}'.format(topic_link))
-                topic_name = response.xpath('//a[@href="'+topic_link+'"]/h4/text()')[0].extract()
-                print('topic_name: {}'.format(topic_name))
-                #dig into link for topic_name
-                #parse_topic
+            item['name'] = parent_topic_name
+            main_link = response.xpath('//div[@id="'+parent_topic_id[:-8]+'0TabContent"]//a[contains(text(), "Main")]//@href')[0].extract()
+            print('main_link: {}'.format(main_link))
+            request = scrapy.Request(main_link, callback=self.parse_parent_main, meta={'item': item})
+            request.meta['item'] = item
+            print('HEYYYYYYYY!!!!!!!!!!!!!!!!!!!!!!!!')
+            #get list of child topics and iterate through that
+            #determine how to lump child item in with parent item
+        return request
 
-    def parse_parent(parent_topic_id):
-        
+    def parse_parent_main(self, response):
+        print('IN PARSE_PARENT_MAIN')
+        item = response.meta['item']
+        item['main_content'] = response.xpath('//div[@id="landingAboutText"]/p/text()')[0].extract()
+        print('main_content: {}'.format(response.xpath('//div[@id="landingAboutText"]/p/text()')[0].extract()))
+        return scrapy.Request(response.url[:-5]+'/about.html', callback=self.parse_parent_about, meta={'item': item})
 
+    def parse_parent_about(self, response):
+        print('IN PARSE_PARENT_ABOUT!!!')
+        item = response.meta['item']
+        about_content = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "textimageText", " " )) and (((count(preceding-sibling::*) + 1) = 1) and parent::*)]//text()').extract()
+        item['about_content'] = ''.join(about_content)
+        subtopics = response.xpath('//*[(@id = "detailContent")]//ul//a/text()').extract()
+        subtopics_items = []
+        for subtopic in subtopics:
+            st = {}
+            st['name'] = subtopic
+            subtopic_description = response.xpath('//*[(@id = "detailContent")]//li[contains(a/text(), "'+subtopic+'")]//text()').extract()[1]
+            st['description'] = subtopic_description
+            subtopics_items.append(st)
+        item['about_child_topics'] = subtopics_items
+        return scrapy.Request(response.url[:-10]+'news.html', callback=self.parse_parent_news, meta={'item': item})
 
+    #only grabbing 2017 news items for now
+    def parse_parent_news(self, response):
+        print('IN PARSE_PARENT_NEWS')
+        item = response.meta['item']
+        articles = response.xpath('//div[@id="listArticlesContainer"]//div[@class="textColumn"]//div[@class="title"]/a/@href').extract()
+        articles_items = []
+        for article in articles:
+            art = {}
+            art['link'] = 'www.census.gov'+article
+            art['title'] = response.xpath('//div[@id="listArticlesContainer"]//div[@class="textColumn"]//div[@class="title"]//a[@href="'+article+'"]/@title').extract()[0].strip()
+            date = response.xpath('//div[@class="article row nested-row no-gutter"]//div[contains(div/a/@href, "'+article+'")]//div[@class="publishdate"]//text()').extract()[0].strip()
+            date = datetime.datetime.strptime(date, '%B %d, %Y').strftime('%m/%d/%Y')
+            art['date'] = date
+            art['description'] = response.xpath('//div[@class="article row nested-row no-gutter"]//div[contains(div/a/@href, "'+article+'")]//div[@class="abstract hidden-xs"]//text()').extract()[0].strip()
+            articles_items.append(art)
+        item['news_items'] = articles_items
+        return scrapy.Request(response.url[:-10]+'/surveys.html', callback=self.parse_parent_survey, meta={'item': item})
 
-        # item['topic_link'] = response.url
-        # item['topic_main_title'] = response.selector.xpath('//h2/text()').extract()[0]
-        # page_content = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "content_block", " " ))]//text()').extract()
-        # item['topic_main_content'] = ''.join(page_content)
-        # item['topic_related_sites'] = ''.join(response.xpath('//*[(@id = "right-column")]//li//text()').extract())
-        # request = scrapy.Request(response.url+'about/', callback=self.parse_about)
-        # request.meta['item'] = item
-        # return request
+    def parse_parent_survey(self, response):
+        print('IN PARSE_PARENT_SURVEY')
+        item = response.meta['item']
+        surveys = response.xpath('//div[@id="listArticlesContainer"]//div[@class="textColumn"]//div[@class="title"]/a/@href').extract()
+        survey_items = []
+        for survey in surveys:
+            sur = {}
+            sur['link'] = survey
+            sur['title'] = response.xpath('//div[@id="listArticlesContainer"]//div[@class="textColumn"]//div[@class="title"]//a[@href="'+survey+'"]/@title').extract()[0].strip()
+            sur['description'] = response.xpath('//div[@class="article row nested-row no-gutter"]//div[contains(div/a/@href, "'+survey+'")]//div[@class="abstract hidden-xs"]//text()').extract()[0].strip()
+            survey_items.append(sur)
+        item['survey_items'] = survey_items
+        return item
 
-    #def parse_parent(self, response):
-
+    def parse_child(self, response):
+        item = ChildTopic()
+        topic_links = response.xpath('//div[@id="'+parent_topic_id+'"]//a/@href').extract()
+        for topic_link in topic_links:
+            #call function for topic
+            item['topic_link'] = topic_link
+            print('topic_link: {}'.format(topic_link))
+            topic_name = response.xpath('//a[@href="'+topic_link+'"]/h4/text()')[0].extract()
+            print('topic_name: {}'.format(topic_name))
+            item['topic_link'] = response.url
+            item['topic_main_title'] = response.selector.xpath('//h2/text()').extract()[0]
+            page_content = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "content_block", " " ))]//text()').extract()
+            item['topic_main_content'] = ''.join(page_content)
+            item['topic_related_sites'] = ''.join(response.xpath('//*[(@id = "right-column")]//li//text()').extract())
+            request = scrapy.Request(response.url+'about/', callback=self.parse_about)
+            request.meta['item'] = item
+            #dig into link for topic_name
+            #parse_topic
+            return request
 
     def parse_about(self, response):
         print('parse about page!!!!')
